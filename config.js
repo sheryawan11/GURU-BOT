34920b5c3f0d90c5e14605c6eaa9b9ce70d0bc07
sheryawan11 <153757792+sheryawan11@users.noreply.github.com>
2023-12-13 22:00:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923287376037', 'GulSher_Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
