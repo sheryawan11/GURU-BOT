7f21d8b47300b51adb4a07bc8209c2f42308b5d2
sheryawan11 <153757792+sheryawan11@users.noreply.github.com>
2023-12-16 12:14:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '🄶🅄🄻 🅂🄷🄴🅁 🄱🄾🅃'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '🅂🄷🄴🅁🅈' 
+global.author = '@GulSher' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
